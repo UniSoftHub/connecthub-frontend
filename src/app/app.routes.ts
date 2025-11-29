71fbb4a832b36359b7142c321b59304f0aa0d481
MrClaro <adryan.contatoprofissional@gmail.com>
2025-11-29 02:35:03 -0300

    feat(routes): implementa rotas principais da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,23 @@ import { Layout } from './components/core/layout/layout';
 import { Home } from './pages/home/home';
 import { Discussions } from './pages/discussions/discussions';
 import { NewDiscussion } from './pages/new-discussion/new-discussion';
+import { Users } from './pages/users/users';
+import { Auth } from './pages/auth/auth';
+import { authGuard } from './guards/auth.guard';
+import { loginAuthGuard } from './guards/login-auth-guard';
+import { Projects } from './pages/projects/projects';
+import { ProjectDetail } from './pages/project-detail/project-detail';
 
 export const routes: Routes = [
   {
+    path: 'auth',
+    component: Auth,
+    canActivate: [loginAuthGuard],
+  },
+  {
     path: '',
     component: Layout,
+    canActivate: [authGuard],
     children: [
       {
         path: '',
@@ -25,6 +37,18 @@ export const routes: Routes = [
       {
         path: 'discussoes/nova',
         component: NewDiscussion,
+      },
+      {
+        path: 'usuarios',
+        component: Users,
+      },
+      {
+        path: 'projetos',
+        component: Projects,
+      },
+      {
+        path: 'projetos/:id',
+        component: ProjectDetail,
       },
     ],
   },
